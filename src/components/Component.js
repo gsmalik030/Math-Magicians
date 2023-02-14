abd95d2081d66e627111da9d171e2dbcc68d29b2
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-14 12:52:50 +0200

    Created UI design for calculator

---
+++ src/components/Component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Component = () => (
+  <div className="calculator">
+    <div className="calculator__display">0</div>
+    <button type="button" className="key">AC</button>
+    <button type="button" className="key">+/-</button>
+    <button type="button" className="key">%</button>
+    <button type="button" className="key orange">+</button>
+    <button type="button" className="key">7</button>
+    <button type="button" className="key">8</button>
+    <button type="button" className="key">9</button>
+    <button type="button" className="key orange">x</button>
+    <button type="button" className="key">4</button>
+    <button type="button" className="key">5</button>
+    <button type="button" className="key">6</button>
+    <button type="button" className="key orange">-</button>
+    <button type="button" className="key">1</button>
+    <button type="button" className="key">2</button>
+    <button type="button" className="key">3</button>
+    <button type="button" className="key orange">+</button>
+    <button type="button" className="key span-2">0</button>
+    <button type="button" className="key">.</button>
+    <button type="button" className="key orange">=</button>
+  </div>
+);
+
+export default Component;
