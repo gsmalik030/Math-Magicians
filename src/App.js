eb91523fa585b696fcaafa035fa0e78feb145993
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-14 13:08:05 +0200

    Added calculator component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
