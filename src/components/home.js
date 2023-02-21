327360a374e2e9617fdb293a7c044dc17d12968f
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-21 12:54:53 +0200

    Added Home page

---
+++ src/components/home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <h1>Welcome to our Page</h1>
+    <p className="home-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+      ipsum asperiores libero exercitationem veritatis aspernatur, voluptatum
+      praesentium unde distinctio dolores dolore tempore, magni sit beatae optio
+      quidem! Odit neque perferendis, corrupti aliquid iusto voluptatem repellat
+      praesentium eius temporibus, nam odio numquam culpa qui vitae eligendi
+      similique modi. Labore excepturi molestias autem quam odit corporis.
+      Necessitatibus sit labore excepturi atque in quos ipsam ducimus cumque
+      quas perferendis, nam culpa ullam veniam repellendus quidem, dolores sunt
+      doloribus quia dolorum alias at maxime, facere distinctio? Vitae eligendi
+      quasi voluptas provident exercitationem. Quis laborum fugiat esse
+      assumenda quidem dignissimos, ea provident itaque voluptates. Enim?
+    </p>
+    <p className="home-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+      ipsum asperiores libero exercitationem veritatis aspernatur, voluptatum
+      praesentium unde distinctio dolores dolore tempore, magni sit beatae optio
+      quidem! Odit neque perferendis, corrupti aliquid iusto voluptatem repellat
+      praesentium eius temporibus, nam odio numquam culpa qui vitae eligendi
+      similique modi. Labore excepturi molestias autem quam odit corporis.
+      Necessitatibus sit labore excepturi atque in quos ipsam ducimus cumque
+      quas perferendis, nam culpa ullam veniam repellendus quidem, dolores sunt
+      doloribus quia dolorum alias at maxime, facere distinctio?
+    </p>
+  </div>
+);
+
+export default Home;
