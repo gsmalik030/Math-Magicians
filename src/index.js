a2fc8065a6a66008665862e2ccca32331573bbda
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-21 12:57:05 +0200

    Added router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
