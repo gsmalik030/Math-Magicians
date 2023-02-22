db7775387b60c5dd1c7dace4a313527fabf6fcd8
Victoria Tumwebaze <69035525+Victoria129@users.noreply.github.com>
2023-02-22 15:59:35 +0300

    Update snapshottests.test.js

--- src/__tests__/snapshottests.test.js
+++ src/__tests__/snapshottests.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
-import Home from '../components/Home';
-import Quote from '../components/Quote';
+import Home from '../components/Home.js';
+import Quote from '../components/Quote.js';
 
 describe('Snapshot for Calculator and Home', () => {
   it('Snapshot should match component render', () => {
