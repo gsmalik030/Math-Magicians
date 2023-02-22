2a358a09c5c25e4dc3d8fa88a3a676ba901e5786
Victoria Tumwebaze <69035525+Victoria129@users.noreply.github.com>
2023-02-22 16:58:12 +0300

    Update snapshottests.test.js

--- src/__tests__/snapshottests.test.js
+++ src/__tests__/snapshottests.test.js
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react';
 import Home from '../components/home';
 import Quote from '../components/quote';
+import Navbar from '../components/Navbar';
+import Calculator from '../components/Calculator';
 
 describe('Snapshot for Calculator and Home', () => {
   it('Snapshot should match component render', () => {
@@ -10,5 +12,13 @@ describe('Snapshot for Calculator and Home', () => {
   it('Snapshot should match component render', () => {
     const quoteview = render(<Quote />);
     expect(quoteview).toMatchSnapshot();
+  });
+  it('Snapshot should match component render', () => {
+    const navview = render(<Navbar />);
+    expect(navview).toMatchSnapshot();
+  });
+  it('Snapshot should match component render', () => {
+    const calcview = render(<Calculator />);
+    expect(calcview).toMatchSnapshot();
   });
 });
